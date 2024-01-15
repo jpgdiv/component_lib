2df1c30cff9d7ff86d66b921bb135ed727d729f5
jpgdiv <peter@divotion.com>
2024-01-15 11:56:40 +0100

    add interaction test

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,7 @@ const config: StorybookConfig = {
     "../components/**/*.mdx",
     "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", '@storybook/addon-interactions',],
   framework: {
     name: "@storybook/web-components-vite",
     options: {},
--- components/myButton/my-button.stories.ts
+++ components/myButton/my-button.stories.ts
@@ -1,5 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
 import { html } from "lit";
+import { userEvent, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import { screen } from "shadow-dom-testing-library"
 
 import "./my-button";
 
@@ -12,4 +15,23 @@ type Story = StoryObj;
 
 export const MyButton: Story = {
   render: () => html`<my-button name="World"></my-button>`,
+};
+
+export const ClickedButton: Story = {
+  render: () => html`<my-button name="World"></my-button>`,
+  play: async ({ canvasElement }) => {
+    // const canvas = within(canvasElement);
+
+        
+    await userEvent.click(screen.getByShadowRole("button"));
+    // await userEvent.click(canvas.getByRole('button'));
+    // await userEvent.click(canvas.getByRole('button'));
+
+    // 👇 Assert DOM structure
+    await expect(
+      screen.getByShadowText(
+        'Click Count: 2'
+      )
+    ).toBeInTheDocument();
+  },
 };
